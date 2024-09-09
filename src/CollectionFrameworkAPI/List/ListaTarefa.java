2d58e1a5b439f004b66c13d65733484df9c6cc8d
jayce <thiagoralmeida23@gmail.com>
2024-09-09 12:24:36 -0300

    Atividade

---
+++ src/CollectionFrameworkAPI/List/ListaTarefa.java
@@ -0,0 +1,35 @@
+package CollectionFrameworkAPI.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        this.tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefaParaRemover = new ArrayList<>();
+        for (Tarefa t : this.tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaRemover.add(t);
+            }
+        }
+        this.tarefaList.removeAll(tarefaParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return this.tarefaList.size();
+    }
+
+    public void obterDescricaoTarefa() {
+        System.out.println(tarefaList);
+    }
+}
---
+++ src/CollectionFrameworkAPI/List/Tarefa.java
@@ -0,0 +1,14 @@
+package CollectionFrameworkAPI.List;
+
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
